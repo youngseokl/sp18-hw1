47afacf01a17c68c1ca5a540bc0c52ca37c553e7
Young Seok Lee <youngseokl@berkeley.edu>
2018-02-15 18:28:10 -0800

    finished

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,8 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a.map!{|item| item.to_i + 2 }.reject{|i| i % 2 != 0}
+    a.reject{|i| i <= 10}.sum-2
+    
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,16 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  (a+b)**2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a.map!{|item| item + 1 }.sort
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  first_name + " " + last_name
 end
 
 def blockin_time(a)
@@ -21,32 +21,33 @@ end
 
 def scrabble(word)
   values = {
-    a: 1,
-    b: 3,
-    c: 3,
-    d: 2,
-    e: 1,
-    f: 4,
-    g: 2,
-    h: 4,
-    i: 1,
-    j: 8,
-    k: 5,
-    l: 1,
-    m: 3,
-    n: 1,
-    o: 1,
-    p: 3,
-    q: 10,
-    r: 1,
-    s: 1,
-    t: 1,
-    u: 1,
-    v: 4,
-    w: 4,
-    x: 8,
-    y: 4,
-    z: 10,
+    "a" => 1,
+    "b" => 3,
+    "c" => 3,
+    "d" => 2,
+    "e" => 1,
+    "f" => 4,
+    "g" => 2,
+    "h" => 4,
+    "i" => 1,
+    "j" => 8,
+    "k" => 5,
+    "l" => 1,
+    "m" => 3,
+    "n" => 1,
+    "o" => 1,
+    "p" => 3,
+    "q" => 10,
+    "r" => 1,
+    "s" => 1,
+    "t" => 1,
+    "u" => 1,
+    "v" => 4,
+    "w" => 4,
+    "x" => 8,
+    "y" => 4,
+    "z" => 10,
   }
   # Q5 CODE HERE
+  word.split("").map!{|item| values[item]}.sum
 end
